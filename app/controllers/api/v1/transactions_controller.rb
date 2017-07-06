f1b2feba6aef04ae01ff28f23c95a39bd9bdd9b8
github username <github email address>
2017-07-06 15:25:01 -0400

    take 2

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,34 +4,16 @@ class Api::V1::TransactionsController < ApplicationController
 								origin_ammount: 10,
 								destination_amount: 1	}
 
-	def create
-
 		origin_account = CurrencyAccount.find(params_mock_object[:origin_currency_account_id])
 		destination_account = CurrencyAccount.find(params_mock_object[:destination_currency_account_id])
 		origin_currency = Currency.find(origin_account.currency_id)
 		destination_currency = Currency.find(destination_account.currency_id)
-			
 
-			if origin_account.balance >=params_mock_object[:origin_amount]
-
+	def create(origin_currency_account_id, destination_currency_account_id, origin_amount, destination_amount)
+		if origin_account.balance >= params_mock_object[:origin_amount]
+			origin_account.balance = origin_account.balance - params_mock_object[:origin_amount] 
+			&&
+			destination_account.balance = destination_account.balance + params_mock_object[:destination_amount]
+		end	
 	end
-
-
 end
-
-
-
-class TransactionController < ApplicationController
-  def create
-    params_mock_object = {origin_currency_account_id:1,destination_currency_account_id:2,origin_amount:10,destination_amount:1}
-    origin_account=CurrencyAccount.find(params_mock_object[:origin_currency_account_id])
-    destination_account=CurrencyAccount.find(params_mock_object[:destination_currency_account_id])
-    #origin_currency = Currency.find(origin_account.currency_id)
-    #destination_currency = Currency.find(destination_account.currency_id)
-    if origin_account.balance >= params_mock_object[:origin_amount]
-      origin_account.balance = origin_account.balance - params_mock_object[:origin_amount]
-      destination_account.balance = destination_account.balance + params_mock_object[:destination_amount]
-    end
-  end
-end
-
